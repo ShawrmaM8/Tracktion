e0cb464271559f92b510372b16a08b0787a2284f
Winorrepeat <155412365+ShawrmaM8@users.noreply.github.com>
2025-09-04 14:35:10 +0300

    Delete electron directory

--- electron/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  invoke: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
-});
